4668b4c762f103ab1c0a2648a036829bc9037724
kevinLinsC <kevin.l.andrade@aluno.senai.br>
2024-10-11 16:17:11 -0300

    SA – ESCOLA SENAI - Exercicio 5-8 completos

--- SA – ESCOLA SENAI.sql
+++ SA – ESCOLA SENAI.sql
@@ -35,34 +35,74 @@ CREATE TABLE matricula(
     FOREIGN KEY (id_curso) REFERENCES curso(id_curso)
 );
 
-SELECT * FROM campus;
-SELECT * FROM curso;
-
--- right join
-SELECT l.titulo, a.name
-FROM livros as l
-RIGHT JOIN autores as a
-ON a.autor_id = l.autor_id;
-
--- 1. Lista de todos os cursos do campus de Vitória
-SELECT cu.nome_curso, ca.cidade
-FROM curso AS cu
+-- 1. Lista de todos os cursos do campus de Vitória - PRONTO
+SELECT c.nome_curso, ca.cidade
+FROM curso AS c
 INNER JOIN campus as ca
-ON ca.cidade = "Vitória";
+ON c.id_campus = ca.id_campus
+WHERE ca.cidade = "Vitória";
 
--- 2. Lista de todos os cursos em ordem Alfabética
-SELECT cu.nome_curso
-FROM curso AS cu
+-- 2. Lista de todos os cursos em ordem Alfabética - PRONTO
+SELECT c.nome_curso
+FROM curso AS c
 ORDER BY nome_curso;
 
--- 3. Quais os 5 cursos mais caros?
-SELECT cu.nome_curso, cu.valor
-FROM curso AS cu
-ORDER BY valor DESC LIMIT 5;
+-- 3. Quais os 5 cursos mais caros? - PRONTO
+SELECT c.nome_curso, c.valor
+FROM curso AS c
+ORDER BY c.valor DESC LIMIT 5;
 
--- 4. Qual curso é o mais barato no Campus da Serra?
-SELECT cu.nome_curso, cu.valor, ca.cidade
-FROM curso AS cu
+-- 4. Qual curso é o mais barato no Campus da Serra? - PRONTO
+SELECT c.nome_curso, c.valor, ca.cidade
+FROM curso AS c
 INNER JOIN campus as ca
-WHERE ca.cidade = "Serra"
-ORDER BY cu.valor;
+ON c.id_campus = ca.id_campus
+WHERE ca.cidade LIKE "Serra"
+ORDER BY c.valor ASC LIMIT 1;
+
+-- 5. Qual o turno com mais cursos disponíveis? - PRONTO
+SELECT c.turno, count(c.turno) as contagem
+FROM curso as c
+GROUP BY c.turno LIMIT 1;
+
+-- 6. Quantos cursos duram mais de dois anos e meio? - PRONTO
+SELECT count(c.nome_curso) as cursos_mais_dois_e_meio
+FROM curso as c
+WHERE c.duracao > 5;
+
+-- 7. Quais os cursos com maior quantidade de alunos inscritos? - TERMINAR
+
+
+-- 8. Qual a média de preço dos cursos listados? - PRONTO
+SELECT sum(c.valor) / count(c.valor) as media_preco
+FROM curso as c;
+
+-- 9. Quais cursos duram mais tempo?
+
+-- 10. Quantos alunos estão matriculados em cada turno?
+
+-- 11. Qual o campus com mais cursos?
+
+-- 12. Quais cursos não possuem alunos cadastrados?
+
+-- 13. Quem se matriculou em 2021?
+
+-- 14. Qual a data de matrícula da aluna “Fernanda Lima”?
+
+-- 15. Quais alunos não se cadastraram em nenhum curso?
+
+-- 16. Quantas alunas matriculadas até o momento?
+
+-- 17. Quais alunos estão matriculados 3 cursos?
+
+-- 18. Qual o curso do aluno “Guilherme Costa”?
+
+-- 19. Quais os alunos matriculados em “Ciência da computação”
+
+-- 20. Relação completa de todos os alunos e seus cursos
+
+
+SELECT * FROM campus;
+SELECT * FROM curso;
+SELECT * FROM matricula;
+SELECT * FROM aluno;
