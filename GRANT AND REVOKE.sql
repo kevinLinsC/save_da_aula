a85c0b6efc28c6ce348a22c473afc78654555a89
KevinLinsC <kevin.l.andrade@aluno.senai.br>
2024-10-24 20:50:45 -0300

    GRANT AND REVOKE script mysql

---
+++ GRANT AND REVOKE.sql
@@ -0,0 +1,31 @@
+-- MOSTRA O USUÁRIO LOGADO NO MOMENTO
+SELECT session_user();
+
+-- VER TABELA DE USUÁRIOS
+SELECT * FROM mysql.user;
+SELECT * FROM mysql.db;
+SELECT * FROM mysql.tables_priv;
+SELECT * FROM mysql.columns_priv;
+
+-- CRIAR UM USUÁRIO
+CREATE USER "cristiano"@"localhost";
+CREATE USER "adm"@"localhost" IDENTIFIED BY "12349";
+
+-- MOSTRA OS PRIVILÉGIOS DE UM USUÁRIO
+SHOW GRANTS FOR "adm"@"localhost";
+
+-- AUTORIZA ACESSAR BANCO ESPECÍFICO
+GRANT ALL ON escolasenai.* TO "adm"@"localhost"
+WITH GRANT OPTION	;
+
+-- RETIRA OS PRIVILEGIOS
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM "adm"@"localhost";
+
+-- RECARREGA OS PRIVILEGIOS
+FLUSH PRIVILEGES;
+
+-- DELETAR USUÁRIO DA TABELA
+DROP USER "cristiano"@"localhost";
+
+-- CONCEDE ACESSOS ESPECÍFICOS
+GRANT CREATE ON escolasenai.*
